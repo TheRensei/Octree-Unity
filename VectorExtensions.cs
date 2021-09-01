02e9e39efbdb402662d7dcd2e54a462c5ce2acd0
Rensei <32361344+TheRensei@users.noreply.github.com>
2021-09-01 16:46:01 +0100

    Add files via upload

---
+++ VectorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class VectorExtensionsMethods
+{
+    public static Vector3 AddFloat(this Vector3 v, float f)
+    {
+        return new Vector3(v.x + f, v.y + f, v.z + f);
+    }
+    public static Vector3 SubtractFloat(this Vector3 v, float f)
+    {
+        return new Vector3(v.x - f, v.y - f, v.z - f);
+    }
+}
